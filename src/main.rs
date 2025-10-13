8e6998b766ae440a92a2ae396152b5b9c6cd6556
username000101 <cxx.username000101@gmail.com>
2025-10-13 22:25:42 +0300

    Call safety::interface::set_handlers()

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 pub mod auth;
 pub mod config;
+pub mod core;
+
 pub mod cli;
 pub mod td;
 pub mod logger;
-pub mod core;
 pub mod server;
+pub mod safety;
 
 #[tokio::main]
 async fn main() {
     let sub2_uptime_start = std::time::Instant::now();
+    safety::interface::set_handlers();
     logger::log::setup_logging();
     tokio::spawn(server::rpc::interface::start_rpc_server());
     cli::interface::parse_args();
