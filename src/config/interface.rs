b6d911da30a44dd0e779c0d05a8469ffe18f85b6
username000101 <cxx.username000101@gmail.com>
2025-10-16 02:32:42 +0300

    Add interface function for crate::config::run_modules::sub2_config_run_modules

--- src/config/interface.rs
+++ src/config/interface.rs
@@ -1,8 +1,16 @@
 use parking_lot::RwLock;
 use once_cell::sync::Lazy;
 use std::path::PathBuf;
-use crate::config::decl::{Configuration, CONFIGURATION};
-use crate::config::parse::parse_config;
+use crate::{
+    config::{
+        decl::{
+            Configuration,
+            CONFIGURATION,
+        },
+        parse::parse_config,
+        run_modules::sub2_config_run_modules,
+    },
+};
 
 pub fn get_config() -> Option<&'static Lazy<RwLock<Configuration>>> {
     match CONFIGURATION.read().ready {
@@ -10,6 +18,8 @@ pub fn get_config() -> Option<&'static Lazy<RwLock<Configuration>>> {
         false => None
     }
 }
+
+pub fn run_modules() { sub2_config_run_modules(); }
 
 pub fn parse(cfg: PathBuf) {
     parse_config(cfg);
