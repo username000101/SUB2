cb86dc3322c731e1a823def4364d432d61c516c4
username000101 <cxx.username000101@gmail.com>
2025-10-20 23:15:57 +0300

    Remove unused code

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,9 @@ pub mod safety;
 
 #[tokio::main]
 async fn main() {
-    let sub2_uptime_start = std::time::Instant::now();
     safety::interface::set_handlers();
     logger::log::setup_logging();
     tokio::spawn(server::rpc::interface::start_rpc_server());
     cli::interface::parse_args();
     core::interface::start_main_loop();
-    tracing::info!("Uptime: {}", sub2_uptime_start.elapsed().as_millis());
 }
